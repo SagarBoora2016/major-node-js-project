cf1f1e84c39bdd77dfc748767a4a4d923abf9187
sagar <sagarboora.2016@gail.com>
2020-05-12 00:36:22 +0530

    Added mongoose and connected to databse successfully

---
+++ config/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/sociel_development");
+const db=mongoose.connection;
+db.on('error', console.error.bind(console, 'cError in connecting to Mongodb'));
+
+db.once("open",function(){
+    console.log("Connected to MongoDb Successfully");
+});
+module.exports = db;
