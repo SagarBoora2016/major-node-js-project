eed7003077cb70e28cf18b78dbac26e0fd209678
sagar <sagarboora.2016@gail.com>
2020-05-13 01:05:40 +0530

    Learned to use Cookies

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
-module.exports.home= function(re1,res){
+module.exports.home= function(req,res){
     console.log("home");
+    console.log(req.cookies);
+    res.cookie("user_id","100");
+    res.cookie("something","thats rifht 2 times");
     return res.render("home",{
         title: "This is my Title."
     });
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const cookieParser = require("cookie-parser");
 const expressLayouts = require('express-ejs-layouts');
 const db = require("./config/mongoose");
+
+app.use(express.urlencoded());;
+app.use(cookieParser());
 app.use(expressLayouts);
 app.set("layout extractStyles",true);
 app.set("layout extractScripts",true);
