a0c4f7f74496550a17431518b95bcf19254ffef1
sagar <sagarboora.2016@gail.com>
2020-05-13 01:49:24 +0530

    user can now sign up

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,8 @@
 module.exports.home= function(req,res){
     console.log("home");
-    console.log(req.cookies);
-    res.cookie("user_id","100");
-    res.cookie("something","thats rifht 2 times");
+    // console.log(req.cookies);
+    // res.cookie("user_id","100");
+    // res.cookie("something","thats rifht 2 times");
     return res.render("home",{
         title: "This is my Title."
     });
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,4 @@
+const User = require("../models/user-sign-up");
 module.exports.profile = function(req,res){
     // res.end("<h1>USer PRofile</h1>");
     console.log("Users profile");
@@ -16,8 +17,39 @@ module.exports.signIn = function(req,res){
     });
 }
 module.exports.createSession =function(req,res){
+    
+
     //To Do
 }
 module.exports.create = function(req,res){
     //TO Do
+    // console.log('here');
+    // console.log(req.body);
+    // console.log(req.body.password);
+    // console.log(req.body.confirm_password);
+    
+    if(req.body.password != req.body.confirm_password){
+        console.log("Passwords Not Equal");
+        return res.redirect("back");
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log("Error in finding user");
+            return res.redirect("back");
+        }
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log("Error in creating user");
+                    console.log(err);
+                    return res.redirect("back");
+                }
+                console.log("User Created");
+                return res.redirect("/users/sign-in");
+            });
+        }else{
+            console.log("User already exists");
+            return res.redirect("back");
+        }
+    }); 
 }
