8a09a2730309fac010ffec84a82acfbc70e79596
sagar <sagarboora.2016@gail.com>
2020-05-15 20:45:08 +0530

    added scss in the project

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ app.use(express.static("./assets"));
 app.set("view engine","ejs");
 app.set("views","./views");
 const MongoStore = require('connect-mongo')(session);
-
+const sassMiddleware = require("node-sass-middleware");
+app.use(sassMiddleware({
+    src:"./assets/scss",
+    dest:"./assets/css",
+    debug:true,
+    outputStyle:"extended",
+    prefix:"/css"
+}));
 app.use(session({
     name:"sociel",
     secret:"bbkivines",
