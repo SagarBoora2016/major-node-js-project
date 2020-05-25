310337d09fe998fc94e3505b8e278e7bab1df916
sagar <sagarboora.2016@gail.com>
2020-05-26 04:32:26 +0530

    No need to refresh after adding post

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,13 +10,36 @@
                 data: newPostForm.serialize(),
                 success:function(data){
                     console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    console.log(newPost);
+                    $("#posts-list-contianer>ul").append(newPost);
                 },
                 error:function(err){
                     console.log(err.responseText);
                 }
-                
+
             });
         });
+    }
+    let newPostDom = function(post){
+        return $(`<li id="post-${post.id}">
+                <a href="/post/delete/${post.id}">
+                    X
+                </a>
+                ${post.content} By ${post.user.name}
+                <div class="post-comment-list">
+                </div>
+                    <div class="post-comment">
+                        <!-- form for comment -->
+                        <form method="POST" action="/comment/create">
+                                <input type="text" placeholder="Enter your comment" name="content" required>
+                                <input type="hidden" name="postid" value="<%=post._id%>" required >
+                                <input type="submit" value="Add Comment">
+                        </form>
+                    </div>
+        
+                </li>
+        `);
     }
     createPost();
 }
