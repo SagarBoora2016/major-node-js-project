f0eab75ed93d7831d663979016dc280da520c3d4
sagar <sagarboora.2016@gail.com>
2020-06-06 08:15:48 +0530

    Added html template for emails

--- mailers/comments_mailers.js
+++ mailers/comments_mailers.js
@@ -1,17 +1,17 @@
 const nodeMailer = require("../config/node-mailer");
 exports.newComment=(comment)=>{
-    console.log("Inside mailer");
+    let htmlString = nodeMailer.renderTemplete({comment:comment},"/comment/newcomment.ejs");
     nodeMailer.transporter.sendMail({
         from:"sagarboora.2020@gmail.com",
         to:comment.user.email,
         subject:"New comment Published",
-        html:"<h1>Yup, Your comment is published.</h1>"
+        html:htmlString
     },(err,info) =>{
         if(err){
             console.log("error");
             return;
         }
-        console.log("mail delivered ",info);
+        // console.log("mail delivered ",info);
         return;
     }
     
