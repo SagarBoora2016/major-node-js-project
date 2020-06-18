9919537e4e72ead2aaf6a02ae78729ce44b75b83
sagar <sagarboora.2016@gail.com>
2020-06-18 13:11:01 +0530

    Chat is now working successfully

--- assets/js/chat-engine.js
+++ assets/js/chat-engine.js
@@ -11,8 +11,44 @@ class ChatEngine{
         }
     }
     connectionHandler(){
+        let self = this;
         this.socket.on("connect",function(){
-            console.log("connection established");
+            // console.log("connection established");
+            self.socket.emit("join_room",{
+                user_email:self.userEmail,
+                chat_room:"Codeial"
+            });
+        });
+        self.socket.on("user_joined",function(data){
+            console.log("user joined",data);
+        });
+        $("#new-message-btn").click(function(){
+            console.log("clicked");
+            let msg = $("#new-message-ip").val();
+            console.log(msg);
+            if(msg!=""){
+                self.socket.emit("send_message",{
+                    user_email:self.userEmail,
+                    message:msg,
+                    chat_room:"Codeial"
+                });
+            }
+        });
+        self.socket.on("receive_message",function(data){
+            // console.log("message Received ",data);
+            let newMessage = $("<li>");
+            let msgType="other-msg";
+            if(data.user_email==self.userEmail){
+                msgType="self-msg"
+            }
+            newMessage.append($("<span>",{
+                "html":data.message
+            }));
+            newMessage.append("<sub>",{
+                "html":data.user_email
+            });
+            newMessage.addClass(msgType);
+            $("#chat-list").append(newMessage);
         });
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,9 +1,18 @@
 module.exports.chatSockets= function(socketServer){
     let io = require("socket.io")(socketServer);
     io.sockets.on("connection",function(socket){
-        console.log("new connection ", socket.id);
+        // console.log("new connection ", socket.id);
+        socket.on("disconnect",function(){
+            console.log("Socket Disconnected.");
+        });
+        socket.on("join_room",function(data){
+            // console.log("Joining Request ",data);
+            socket.join(data.chat_room);
+            io.in(data.chat_room).emit("user_joined",data);
+        });
+        socket.on("send_message",function(data){
+            // console.log("inside send msg ",data);
+            io.in(data.chat_room).emit("receive_message",data);
+        });
     });
-    io.sockets.on("disconnect",function(){
-        console.log("Socket Disconnected.");
-    });
-};
+}
