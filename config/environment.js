acce2fbc4d53518641cafe6a203c5506385f4570
sagar <sagarboora.2016@gail.com>
2020-06-27 15:49:45 +0530

    added environment variables

--- config/environment.js
+++ config/environment.js
@@ -22,8 +22,27 @@ const development = {
     user_api:"sociel"
 }
 
-// const production = {
-//     name="production"
-// }
+const production = {
+    name:"production",
+    src_path:process.env.CODEIAL_SRC_PATH,
+    dest_path:process.env.CODEIAL_DEST_PATH,
+    static_path:process.env.CODEIAL_STATIC_PATH,
+    db:process.env.CODEIAL_DB,
+    smtp:{
+        service:"gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.CODEIAL_GMAIL_USER, // generated ethereal user
+            pass: process.env.CODEIAL_GMAIL_PASSWORD // generated ethereal password
+        }
+    },
+    google_clientId:process.env.CODEIAL_GMAIL_CLIENT_ID,
+    google_secret:process.env.CODEIAL_GOOGLE_SECRET,
+    google_callbackURL:process.env.CODEIAL_GOOGLE_CALLBACKURL,
+    secretOrKey:process.env.CODEIAL_SECRET_OR_KEY,
+    user_api:process.env.CODEIAL_USER_API
+}
 
 module.exports = development;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-
+console.log(process.env.Port);
 const env = require("./config/environment");
 
 const cookieParser = require("cookie-parser");
